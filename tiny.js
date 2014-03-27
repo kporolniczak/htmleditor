ea06673a6fed9c9efb40fde6defd967ceb34cab6
Krzysztof Porolniczak <s9256@pjwstk.edu.pl>
2014-03-27 22:54:36 +0100

    Nomad tags

--- tiny.js
+++ tiny.js
@@ -1,4 +1,4 @@
-var output = '<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE course SYSTEM "nomadCourse.dtd">\n';
+﻿var output = '<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE course SYSTEM "nomadCourse.dtd">\n';
 
 function buildOutput(){
 	var type = "remote";
@@ -24,11 +24,13 @@ function buildOutput(){
 tinyMCE.init({
 			plugins: ["advlist anchor autoresize charmap code directionality emoticons fullscreen hr nonbreaking paste preview print wordcount searchreplace save table visualchars visualblocks textcolor"],	
 			extended_valid_elements : 'lesson,screen',
-			custom_elements: 'lesson,screen',
-			toolbar1: "ltr rtl | emoticons | nonbreaking | preview |  searchreplace | visualchars | forecolor backcolor | fullscreen | ",
-			toolbar2: "Nomad  | save",			
+			custom_elements: 'lesson,screen,title,text',
+			toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+			toolbar2: " print preview media | forecolor backcolor emoticons | Nomad  | save",			
 			language: 'pl',
 			content_css : "style.css",
+			force_br_newlines : true,
+			force_p_newlines : false,
 			save_enablewhendirty: true,
 			save_onsavecallback: function() {
 			buildOutput();			
@@ -37,19 +39,23 @@ tinyMCE.init({
 			saveAs(blob, cname);
 			},
 			selector: '#content', 
+			forced_root_block : false,
 			nonbreaking_force_tab: true,
 			setup: function(editor) {
 				editor.addButton('Nomad', {
-            type: 'menubutton',
-            text: 'Nomad',
-            icon: false,
-            menu: [
-                {text: 'Lekcja', onclick: function() {editor.insertContent('<lesson>---Lekcja---</lesson>');}},
-                {text: 'Ekran', onclick: function() {editor.insertContent('<screen>---Ekran---</screen>');}}
-            ]
-        });
-    }
+					type: 'menubutton',
+					text: 'Nomad',
+					icon: false,
+					menu: [
+						{text: 'Lekcja', onclick: function() {					
+						editor.insertContent('<lesson><hr>---W tym miejscu można dodawać kolejne ekrany---<hr></lesson><br>');}},
+						{text: 'Ekran', onclick: function() {
+						var title = prompt("Podaj nazwę ekranu","Nazwa ekranu");
+						editor.insertContent('<screen><title>'+title+'</title><br><text>Zawartość ekranu</text></screen>');}}
+					]
+				});
+			}
 			});
-			
-$(document).ready(function(){
+	
+$(document).ready(function(){		
 });
